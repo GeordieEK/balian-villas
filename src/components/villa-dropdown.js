13025384d997a76d02677badada7aa0898a61014
GeordieEK <geordieek@gmail.com>
2023-02-08 12:32:24 +1100

    Stopped dropdown pushing other divs

--- src/components/villa-dropdown.js
+++ src/components/villa-dropdown.js
@@ -3,28 +3,31 @@ import Link from 'next/link';
 import { BiChevronDown } from 'react-icons/bi';
 
 const VillaDropdown = () => {
-  const [toggle, setToggle] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   return (
-    <div className="font-medium w-24 h-24">
+    <div className="font-medium">
       <div
-        className="w-full p-1 flex items-center justify-content"
-        onClick={() => setToggle(!toggle)}
+        className="w-full flex items-center justify-content"
+        onClick={() => setDropdownOpen(!dropdownOpen)}
       >
         The Villas{' '}
-        <BiChevronDown size={15} className={`${toggle && 'rotate-180'}`} />
+        <BiChevronDown
+          size={15}
+          className={`${dropdownOpen && 'rotate-180'}`}
+        />
       </div>
-      {toggle && (
-        <ul className="mt-2">
+      {dropdownOpen && (
+        <ul className="absolute mt-2">
           <li
             className="p-1 text-sm hover:bg-sky-600"
-            onClick={() => setToggle(false)}
+            onClick={() => setDropdownOpen(false)}
           >
             <Link href="/villa-ganesha">Villa Ganesha</Link>
           </li>
           <li
             className="p-1 text-sm hover:bg-sky-600"
-            onClick={() => setToggle(false)}
+            onClick={() => setDropdownOpen(false)}
           >
             <Link href="/villa-nanas">Villa Nanas</Link>
           </li>
