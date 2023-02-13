b1efbae949113b9be18e13b71ae62af6e16e2af3
GeordieEK <geordieek@gmail.com>
2023-02-13 22:19:10 +1100

    Added content block to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 import { Inter } from '@next/font/google';
 import Carousel from '@/components/carousel';
 import AutoSlider from '@/components/auto-slider';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -11,6 +13,21 @@ export default function Home() {
           <h1 className="text-white text-8xl font-bold tracking-wide text-center absolute top-1/3 left-0 right-0 text-stroke draw-animation">
             THE VILLAS <br /> BALIAN BEACH
           </h1>
+        </div>
+        <div className="flex justify-center">
+          <div className="w-1/2 h-auto p-16 flex justify-center">
+            {/* TODO: Add Alts */}
+            <img src={'/image4.webp'} alt="" loading="lazy" />
+          </div>
+          <div className="w-1/2 px-32 text-center flex justify-center items-center">
+            Welcome to The Villas, two luxurious and peaceful hideaways on
+            Balian Beach, Bali&#39;s best kept secret. Our beautifully appointed
+            villas offer the perfect blend of comfort and serenity, making them
+            the ideal choice for travelers seeking a tranquil escape from the
+            hustle and bustle. The lush tropical surroundings and range of
+            modern amenities provide the perfect setting for a rejuvenating and
+            memorable vacation experience.
+          </div>
         </div>
       </main>
     </>
