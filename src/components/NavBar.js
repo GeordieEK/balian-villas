38812e7922d3d37626b27eb1d899cb6ec18dac8b
GeordieEK <geordieek@gmail.com>
2023-02-23 22:35:37 +1100

    Stylised scrollbar, added relative div to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,18 +3,20 @@ import VillaDropdown from './villa-dropdown';
 
 const NavBar = () => {
   return (
-    <div className="absolute top-2 left-0 w-full">
-      <div className="mx-auto flex justify-center space-x-32 text-stroke-[1]">
-        <Link href="/" className="font-medium">
-          Home
-        </Link>
-        <Link href="/about" className="font-medium">
-          About
-        </Link>
-        <VillaDropdown />
-        <Link href="/photogallery" className="font-medium">
-          Gallery
-        </Link>
+    <div className="relative">
+      <div className="absolute top-2 left-0 w-full">
+        <div className="mx-auto flex justify-center space-x-32 text-stroke-[1]">
+          <Link href="/" className="font-medium">
+            Home
+          </Link>
+          <Link href="/about" className="font-medium">
+            About
+          </Link>
+          <VillaDropdown />
+          <Link href="/photogallery" className="font-medium">
+            Gallery
+          </Link>
+        </div>
       </div>
     </div>
   );
