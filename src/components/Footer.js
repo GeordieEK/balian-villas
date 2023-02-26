3f92e348307ea796c2f52671e7bdeaa217b6cf56
GeordieEK <geordieek@gmail.com>
2023-02-26 21:04:57 +1100

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,13 @@
 const Footer = () => {
-  return <div className="flex justify-center">Footer</div>;
+  return (
+    <div className="flex justify-center text-center text-sm">
+      {' '}
+      <i>
+        <p>The Villas - Balian Beach.</p>
+        <p>All rights reserved. &copy; </p>
+      </i>
+    </div>
+  );
 };
 
 export default Footer;
