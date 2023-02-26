9938b925b29443e83012509e01c179e287b42832
GeordieEK <geordieek@gmail.com>
2023-02-26 20:37:41 +1100

    Gallery-image more broadly useable

--- src/components/gallery-image.js
+++ src/components/gallery-image.js
@@ -3,31 +3,40 @@ import Image from 'next/image';
 
 const GalleryImage = ({ image }) => {
   const [isLoading, setLoading] = useState(true);
+  const [hover, setHover] = useState(false);
 
   const cn = (...classes) => {
     return classes.filter(Boolean).join(' ');
   };
 
   return (
-    <div className="group">
-      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-        <Image
-          alt=""
-          src={image.src}
-          fill={true}
-          sizes="100vw,
+    <div
+      className="relative group w-full h-full overflow-hidden"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <Image
+        alt=""
+        src={image.src}
+        fill={true}
+        sizes="100vw,
                 50vw,
                 33vw"
-          style={{ objectFit: 'cover' }}
-          className={cn(
-            'duration-700 ease-in-out group-hover:opacity-75',
-            isLoading
-              ? 'scale-110 blur-2xl grayscale'
-              : 'scale-100 blur-0 grayscale-0'
-          )}
-          onLoadingComplete={() => setLoading(false)}
-        />
-      </div>
+        style={{ objectFit: 'cover' }}
+        className={cn(
+          'duration-500 ease-in-out group-hover:opacity-75',
+          isLoading
+            ? 'scale-105 blur-xl grayscale'
+            : 'scale-100 blur-0 grayscale-0'
+        )}
+        onLoadingComplete={() => setLoading(false)}
+      />
+      {/* Display text on hover */}
+      {hover && (
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <p className="text-white">{image.alt}</p>
+        </div>
+      )}
     </div>
   );
 };
