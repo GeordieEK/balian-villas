51ab62c28895e89454622bd469622c58deeb6a50
GeordieEK <geordieek@gmail.com>
2023-02-28 18:10:27 +1100

    CSS Grid re-ordering on re-size on index

--- src/components/two-wide-img-text.js
+++ src/components/two-wide-img-text.js
@@ -1,21 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
 
 const TwoWideImgText = ({ image, alt, text, imageLeft }) => {
+  // If image left
+  let divClass = 'p-8 flex justify-center';
+  if (!imageLeft) divClass += ' lg:order-1';
   return (
-    <div className="flex justify-center mt-full">
-      {imageLeft && (
-        <div className="w-1/2 h-auto p-8 flex justify-center">
-          <img src={image} alt={alt} loading="lazy" />
-        </div>
-      )}
-      <div className="w-1/2 px-32 text-center flex justify-center items-center">
+    <div className="grid grid-cols-1 lg:grid-cols-2 mt-full">
+      <div className={divClass}>
+        <img src={image} alt={alt} loading="lazy" />
+      </div>
+      <div className="px-24 text-center flex justify-center items-center">
         {text}
       </div>
-      {!imageLeft && (
-        <div className="w-1/2 h-auto p-8 flex justify-center">
-          <img src={image} alt={alt} loading="lazy" />
-        </div>
-      )}
     </div>
   );
 };
